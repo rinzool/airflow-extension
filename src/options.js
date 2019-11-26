2dd7b6e858eef9e1fd54074d3e7f42e0406b58da
Quentin NAMBOT <qnambot.externe@m6.fr>
2019-11-26 11:57:55 +0100

    feat: add option to update highlight style

--- src/options.js
+++ src/options.js
@@ -1,6 +1,9 @@
 // Get DOM elements
 const successAlert = document.getElementById("success-alert");
 const dagsTextArea= document.getElementById("dags");
+const hightlightStyleInput = document.getElementById('highlight-dag-style');
+
+const defaultHighlightStyle = 'background: lightgoldenrodyellow; font-weight: bold';
 
 var currentBrowser = typeof InstallTrigger !== 'undefined' ? browser : chrome;
 
@@ -10,20 +13,39 @@ currentBrowser.storage.sync.get('dags', function(data) {
   dagsTextArea.value = data.dags ? data.dags : '';
 })
 
+currentBrowser.storage.sync.get('highlightStyle', function(data) {
+  console.log(data);
+  hightlightStyleInput.value = data.highlightStyle ? data.highlightStyle : defaultHighlightStyle;
+})
+
 // Update stored list of dags
-function updateDags() {
+function update() {
   hide(successAlert);
   
   const dags = dagsTextArea.value.split(',').map(dag => dag.replace(' ', ''));
+  const style = hightlightStyleInput.value;
 
-  currentBrowser.storage.sync.set({dags: dags}, function() {
+  var data = {}
+  if(dags) data.dags = dags;
+  if(style) data.highlightStyle = style;
+
+  console.log(data)
+
+  currentBrowser.storage.sync.set(data, function() {
     show(successAlert);
     setTimeout(function() {hide(successAlert);}, 2000);
   });
 }
 
+// Reset hightlight default style
+function resetHightlightDefault() {
+  hightlightStyleInput.value = defaultHighlightStyle;
+}
+
 // Update stored list of dags when validating form
-document.getElementById('validate').addEventListener('click', updateDags)
+document.getElementById('validate').addEventListener('click', update)
+
+document.getElementById('reset-highlight-default').addEventListener('click', resetHightlightDefault)
 
 // Basic show/hide functions
 function hide(e) {
@@ -33,4 +55,11 @@ function hide(e) {
 function show(e) {
   e.setAttribute("style", "display: block");
 }
+
+// Update when enter
+document.addEventListener('keypress', function(e) {
+  if(e.keyCode == 13) {
+    update();
+  }
+});
 
